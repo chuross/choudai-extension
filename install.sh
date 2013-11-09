7791b3acb275e232786688efe82f8cd651a3b8cb
chuross <azarasi.mo.mo@gmail.com>
2013-11-09 22:57:11 +0900

    install.shとREADMEの内容修正

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
+JSZIP_DIR=choudai/lib/jszip
+
 echo '**************************'
 echo 'インストールを開始します'
 echo '**************************'
 echo 'サブモジュールを更新します'
 git submodule init
 git submodule update
+echo 'フォルダを作成しています'
+if [ ! -d ${JSZIP_DIR} ]; then
+mkdir ${JSZIP_DIR}
+fi
 echo 'jszipをコピーしています'
-cp jszip/jszip.js choudai/lib
+cp jszip/jszip.js ${JSZIP_DIR}
 echo '**************************'
 echo 'インストールが終わりました'
 echo '**************************'
